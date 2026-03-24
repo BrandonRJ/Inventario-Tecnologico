68dea199fa6bd3b3a264e5aacfa658c2539557cc
Brandon Daniel Robles Jimenez <brandon-95cr@hotmail.com>
2026-03-24 15:45:30 -0600

    Creacion de clase InventarioContext.cs y su logica.

---
+++ CapaDatos/InventarioContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using InventarioTecnologia.CapaDatos.Entidades;
+using System.Data.Entity;
+
+namespace InventarioTecnologia.CapaDatos
+{
+    public class InventarioContext : DbContext
+    {
+        // El nombre "InventarioContext" debe coincidir con la cadena de conexión en Web.config
+        public InventarioContext()
+            : base("name=InventarioContext")
+        {
+        }
+
+        // DbSet = representación de una tabla
+        public DbSet<Dispositivo> Dispositivos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
