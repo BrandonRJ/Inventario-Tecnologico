c6adeac3b3dd3a0367c744af80529707d4995652
Brandon Daniel Robles Jimenez <brandon-95cr@hotmail.com>
2026-03-24 15:42:30 -0600

    Creacion de carpeta CapaDatos/Entidades Categoria.cs, Dispositivo.cs y su logica.

---
+++ CapaDatos/Entidades/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventarioTecnologia.CapaDatos.Entidades
+{
+    [Table("Categorias")]
+    public class Categoria
+    {
+        [Key]
+        [Column("IdCategoria")]
+        public int IdCategoria { get; set; }
+
+        [Column("NombreCategoria")]
+        public string NombreCategoria { get; set; }
+
+        // Navegación: una categoría puede tener muchos dispositivos
+        public virtual ICollection<Dispositivo> Dispositivos { get; set; }
+    }
+}
